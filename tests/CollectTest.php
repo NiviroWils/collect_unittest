f3a90bfd649c24889934ff1724e605cccddf6d5c
NiviroWils <145367856+NiviroWils@users.noreply.github.com>
2024-05-11 00:52:56 +0700

    Создание тестов для функций keys(), values() и get()

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,13 +1,36 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Collect\Collect;
 
 class CollectTest extends TestCase
 {
-    public function testCount()
+    protected $testArray;
+
+    protected function setUp(): void
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        parent::setUp();
+        $this->testArray = ['a' => 1, 'b' => 2, 'c' => 3];
     }
 
-}
+    // Тестирование методов keys() и values()
+    public function testKeysAndValuesMethods()
+    {
+        $collect = new Collect($this->testArray);
+        $keys = $collect->keys()->toArray();
+        $values = $collect->values()->toArray();
+
+        $this->assertEquals(['a', 'b', 'c'], $keys);
+        $this->assertEquals([1, 2, 3], $values);
+    }
+
+    // Тестирование метода get()
+    public function testGetMethod()
+    {
+        $collect = new Collect($this->testArray);
+        $value = $collect->get('b');
+
+        $this->assertEquals(2, $value);
+    }}
+
+
